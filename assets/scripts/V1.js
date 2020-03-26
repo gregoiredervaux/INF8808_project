d6a86bb62bde7081c4983c38e42fadc5bd81d0c7
Etienne Boisvenue <etienneboisvenue@gmail.com>
2020-03-26 17:04:30 -0400

    Added sources for tab layout in comment

---
+++ assets/scripts/V1.js
@@ -0,0 +1,24 @@
+//http://www.cagrimmett.com/til/2016/08/19/d3-pie-chart.html
+
+
+// Fonction pour créer piechart_dataset. Retourne le nombre d'incident dans l'intervalle et hors de l'intervalle
+function select_incidents_in_the_timeframe(dataset, begin, end){
+
+};
+
+
+
+
+
+
+
+
+
+
+
+// piechart_dataset est de la forme [{'nombre_incident_dans_intervalle':123},{'nombre_incident_hors_intervalle'}]
+// g est le groupe SVG dans lequel le piechart doit être
+function create_piechart(g, piechart_dataset) {
+    console.log(piechart_dataset);
+
+};
